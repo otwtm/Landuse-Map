d85ddbb986cddcc7d0654bd270624f06ed0ccebc
SebastianWolf <sewo@dtu.dk>
2018-05-04 15:51:57 +0200

    circles now appear on selected country

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 """
 functions used in the land use map
 """
+import math
 
 def calc_area(country, product):
     """
@@ -15,3 +16,17 @@ def km2deg(km):
     converts km to degree (simplified)
     """
     return km / 111.111
+
+def radius_deg(country, product):
+    area = calc_area(country=country, product=product)
+    radius = math.sqrt(area / math.pi)  # [km]
+    r_deg = km2deg(radius)  # [deg]
+    return(r_deg)
+
+def country_center(country):
+    """
+    return the center (median) of a country's coordinates
+    """
+    lat = country.get_coordinates().lat().median()
+    lon = country.get_coordinates().lon().median()
+    return [lat, lon]
--- version0.py
+++ version0.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
-from bokeh.io import show
+import functions as fn
+
+
 from bokeh.models import (
     ColumnDataSource,
     HoverTool,
@@ -19,8 +21,53 @@ from bokeh.models.widgets import Slider
 from bokeh.io import curdoc
 from bokeh.layouts import row, widgetbox
 from bokeh.models.glyphs import Text
+from bokeh.io import output_file, show
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import CheckboxButtonGroup
 
-import functions as fn
+
+# Event handlers
+def my_tap_handler(attr,old,new):
+    global selected_country
+    global slider_vegetarians
+    patch_name = source.data['key'][new['1d']['indices'][0]]
+    print("patch_name: ", patch_name)
+    selected_country = [country for country in countries if country.get_key()==patch_name][0]
+    selected_country.set_veges(slider_vegetarians.value)
+    print(selected_country)
+
+    #x = [fn.country_center(selected_country)[1]]
+    #y = [fn.country_center(selected_country)[0]]
+    #z = [fn.radius_deg(selected_country, beef)]
+    #source_circle.data = dict(x=x,y=y,z=z)
+    update_circle()
+    update_slider_title(selected_country)
+
+def update_slider_title(country):
+    global slider_vegetarians
+    slider_vegetarians.title = "Vegetarians in "+country.get_name()+" in percent"
+
+def button_handler(new):
+    print('button option ' + str(new) + ' selected.')
+
+def update_data(attrname, old, new):
+    global selected_country
+    global slider_vegetarians
+    selected_country.set_veges(slider_vegetarians.value)
+    update_circle()
+    #z = [fn.radius_deg(selected_country, beef)]
+    #x = [fn.country_center(selected_country)[1]]
+    #y = [fn.country_center(selected_country)[0]]
+    #source_circle.data = dict(x=x,y=y,z=z)
+
+def update_circle():
+    global selected_country
+    global source_circle
+    x = [fn.country_center(selected_country)[1]]
+    y = [fn.country_center(selected_country)[0]]
+    z = [fn.radius_deg(selected_country, beef)]
+    source_circle.data = dict(x=x,y=y,z=z)
+
 
 data = pd.read_csv("coordinates.csv")
 
@@ -30,38 +77,40 @@ palette.reverse()
 ger = Country('Germany',350,0,80,[])
 usa = Country('USA',9147,0,325,[])
 usa.set_meat_cons_pc(120)
+ger.set_meat_cons_pc(100)
 usa.set_veges(5)
-print(str(ger))
+ger.set_veges(5)
 countries=[ger,usa]
 for country in countries:
     lat = data[data['key'] == country.get_key()]['lat']
     lon = data[data['key'] == country.get_key()]['lon']
     country.set_coordinates(Coordinates(lat,lon))
 
+selected_country = ger
+
 # initialize products
 beef = Product(name="beef", type="animal")
 beef.set_lifetime(1.5)
 beef.set_landuse_per_kg(20)
 
+# plotting data
 country_xs = [country.get_coordinates().lon() for country in countries]
 country_ys = [country.get_coordinates().lat() for country in countries]
-
 country_names = [country.get_name() for country in countries]
 country_areas = [country.get_area() for country in countries]
+country_keys = [country.get_key() for country in countries]
 
 color_mapper = LogColorMapper(palette=palette)
-
 
 source = ColumnDataSource(data=dict(
     x=country_xs,
     y=country_ys,
     name=country_names,
     area=country_areas,
+    key = country_keys
 ))
 
 TOOLS = "pan,wheel_zoom,reset,hover,save,tap"
-
-
 p = figure(
     title="First Test Map ", tools=TOOLS,
     x_axis_location=None, y_axis_location=None,
@@ -73,14 +122,11 @@ renderer = p.patches('x', 'y', source=source,
           fill_color={'field': 'area', 'transform': color_mapper},
           fill_alpha=0.7, line_color="white", line_width=0.5)
 
-
-
 selected_patches = Patches(fill_color="#a6cee3")
 nonselected_patches = Patches(fill_color="#ffcee3")
 
 renderer.selection_glyph = selected_patches
 renderer.nonselection_glyph = nonselected_patches
-
 
 hover = p.select_one(HoverTool)
 hover.point_policy = "follow_mouse"
@@ -90,64 +136,32 @@ hover.tooltips = [
     ("(long, lat)", "($x, $y)"),
 ]
 
+z = [fn.radius_deg(selected_country, beef)]
+x = [fn.country_center(selected_country)[1]]
+y = [fn.country_center(selected_country)[0]]
+
+source_circle = ColumnDataSource(data=dict(x=x,y=y,z=z))
+p.circle('x','y',radius='z', source=source_circle)
 
 
-usa_center_lat = usa.get_coordinates().lat().median()
-usa_center_lon = usa.get_coordinates().lon().median()
 
-
-area = fn.calc_area(country=usa, product=beef)
-radius = math.sqrt(area / math.pi)  # [km]
-radius_deg = fn.km2deg(radius)
-z = [radius_deg]
-
-
-source_z = ColumnDataSource(data=dict(z=z))
-p.circle(usa_center_lon,usa_center_lat,radius='z', source=source_z)
-
-slider_vegetarians = Slider(title="vegetarians [percent]", value=usa.get_veges(), start=0.0, end=100.0, step=0.1)
-
-area = fn.calc_area(country=usa, product=beef)
-print("area:", area)
-
-def update_data(attrname, old, new):
-
-    # update value
-    #land_use_per_kg = 20
-    #animal_lifetime = 1.5 # [y]
-    # area = share of canivores x population[mil] x cons_per_capita x landuse per kg meat x animal_lifetime
-    #area = (1.0-0.01*slider_vegetarians.value)*usa.get_population() * usa.get_meat_cons_pc() * land_use_per_kg * animal_lifetime # [km2]
-
-    usa.set_veges(slider_vegetarians.value)
-    area = fn.calc_area(country=usa, product=beef)
-
-    print("area:", area)
-    radius = math.sqrt(area/math.pi) # [km]
-    #radius_deg = radius/ 111.111 # [deg]
-    radius_deg = fn.km2deg(radius)
-    z = [radius_deg]
-    source_z.data = dict(z=z)
-
-slider_vegetarians.on_change('value', update_data)
-
-###############
-from bokeh.io import output_file, show
-from bokeh.layouts import widgetbox
-from bokeh.models.widgets import CheckboxButtonGroup
-
-output_file("checkbox_button_group.html")
+# widgets
+slider_vegetarians = Slider(title="Vegetarians in "+selected_country.get_name()+" in percent", value=selected_country.get_veges(), start=0.0, end=100.0, step=0.1)
 
 checkbox_button_group = CheckboxButtonGroup(
         labels=["Option 1", "Option 2", "Option 3"], active=[0, 1])
 
-#show(widgetbox(checkbox_button_group))
 
 
-def my_radio_handler(new):
-    print('Radio button option ' + str(new) + ' selected.')
+# Events
+slider_vegetarians.on_change('value', update_data)
+
+renderer.data_source.on_change("selected", my_tap_handler)
+
+checkbox_button_group.on_click(button_handler)
 
 
-checkbox_button_group.on_click(my_radio_handler)
+
 #######################################
 
 # Set up layouts and add to document
@@ -156,15 +170,8 @@ inputs = widgetbox(slider_vegetarians, checkbox_button_group)
 curdoc().add_root(row(inputs, p, width=800))
 curdoc().title = "Vege Map"
 
-url = "http://www.colors.com"
-def my_tap_handler(attr,old,new):
-    #print('hi there')
-    #p.text(x=0,y=0,text="@name")
-    patch_name =  source.data['name'][new['1d']['indices'][0]]
-    print("TapTool callback executed on Patch {}".format(patch_name))
-    p.text(x=0,y=0,text=[patch_name])
 
-renderer.data_source.on_change("selected", my_tap_handler)
-#taptool = p.select(type=TapTool)
-#taptool.callback = my_tap_handler()
+
+
+
 
